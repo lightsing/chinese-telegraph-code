747db18a5fc9088a6e3d159ed69990874c41cc9b
lightsing <light.tsing@gmail.com>
2018-11-24 22:03:08 +0800

    add commpress

--- src/converter/code2char.rs
+++ src/converter/code2char.rs
@@ -1,4 +1,7 @@
-extern crate lzma;
+extern crate lzma_rs;
+
+use std::io::BufReader;
+use self::lzma_rs::xz_decompress;
 
 static N: usize = 10;
 
@@ -13,8 +16,12 @@ pub fn create_code_node(val: Option<char>) -> Box<CodeNode> {
 }
 
 pub fn init() -> Vec<u8> {
-    match lzma::decompress(include_bytes!("code2char-src.txt.xz")) {
-        Ok(decompressed) => decompressed,
+    let mut bytes: Vec<u8> = include_bytes!("code2char-src.txt.xz").to_vec();
+    // let mut input_buffer = BufReader::new(bytes);
+    let mut output_buffer: Vec<u8> = Vec::new();
+
+    match xz_decompress(&mut BufReader::new(bytes.as_slice()), &mut output_buffer) {
+        Ok(_) => output_buffer,
         Err(error) => {
             panic!("There was a problem decompress the source file: {:?}", error)
         }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod converter;
 
 fn main() {
-    let mut src = converter::code2char::init();
-
+    let mut src: Vec<u8> = converter::code2char::init();
+    let string: Vec<&str> = std::str::from_utf8(src.as_slice()).unwrap()
+        .split('\n')
+        .collect();
+    println!("{:?}", string);
 }
